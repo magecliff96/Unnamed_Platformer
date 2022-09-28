d82a8dc884fbe55d4fb642b0c620ac5ef506573c
Leigh Sena <lsena@ceresimaging.net>
2022-09-27 18:05:05 -0700

    fixed issue #11

--- Unnamed_Platformer/src/Player/States/idle.gd
+++ Unnamed_Platformer/src/Player/States/idle.gd
@@ -22,7 +22,7 @@ func enter() -> void:
 
 #entering different states based on buttons pressed
 func input(event: InputEvent) -> BaseState:
-	if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
+	if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
 		if Input.is_action_pressed("run"):
 			return run_state
 		return walk_state
