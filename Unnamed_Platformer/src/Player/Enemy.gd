f2f038456d6b813ed5a72e9fd9f3ac5cc9be1897
Aramusha416 <pedrocervantes416@gmail.com>
2022-09-28 22:44:31 -0700

    Added Enemy Stomp/Death
    Stomp and death added to enemy

--- Unnamed_Platformer/src/Player/Enemy.gd
+++ Unnamed_Platformer/src/Player/Enemy.gd
@@ -2,7 +2,15 @@ extends "res://src/Player/Actor.gd"
 
 
 func _ready() -> void:
+	set_physics_process(false) #enemy will not load movement until player is in view
 	actor_velocity.x = -actor_speed.x
+	
+	
+	
+func _on_StompDetector_body_entered(body: Node) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	queue_free()
 
 # hits wall, it moves the opposite way
 func _physics_process(delta: float) -> void:
@@ -10,3 +18,9 @@ func _physics_process(delta: float) -> void:
 	if is_on_wall():
 		actor_velocity.x *= -1.0 
 	actor_velocity.y = move_and_slide(actor_velocity, FLOOR_NORMAL).y
+	
+
+
+
+
+
